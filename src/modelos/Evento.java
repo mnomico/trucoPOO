c0773d3ecd0cfc9c1f21dcaae43300dd5d23bf68
nomico <nomicomateou@gmail.com>
2023-11-26 22:51:20 -0300

    update Evento

--- src/modelos/Evento.java
+++ src/modelos/Evento.java
@@ -3,11 +3,13 @@ package modelos;
 public enum Evento {
     MOSTRAR_MENU,
     JUGAR_CARTA,
-    APOSTAR,
+    IRSE_AL_MAZO,
     RESPONDER_APUESTA,
     DIJO_QUIERO,
+    DIJO_NO_QUIERO,
     FIN_RONDA,
     FIN_MANO,
     CAMBIO_TURNO,
     RESULTADO_ENVIDO,
+    FIN_PARTIDA,
 }
