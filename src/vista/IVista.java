f3587f655e381aac8bf644ce4a370d029f35d797
nomico <nomicomateou@gmail.com>
2023-11-27 18:09:42 -0300

    add returnFrameXPos, returnFrameYPos, setLocation

--- src/vista/IVista.java
+++ src/vista/IVista.java
@@ -4,6 +4,12 @@ import controlador.Controlador;
 
 public interface IVista {
 
+    int returnFrameXPos();
+
+    int returnFrameYPos();
+
+    void setLocation(int x, int y);
+
     void setControlador(Controlador controlador);
 
     void mostrarMenuPrincipal();
