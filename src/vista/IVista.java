f8543b59c6d6024ad32e2b4ed66889bcf9db6a87
nomico <nomicomateou@gmail.com>
2023-11-30 18:40:00 -0300

    add output methods

--- src/vista/IVista.java
+++ src/vista/IVista.java
@@ -1,6 +1,7 @@
 package vista;
 
 import controlador.Controlador;
+import modelos.Apuesta;
 
 public interface IVista {
 
@@ -19,5 +20,31 @@ public interface IVista {
     void mostrarPuntos();
 
     void mostrarCartas();
+
+    // FUNCIONES PARA MOSTRAR
+
+    public void mostrarCartaJugada(String jugadorActual, String cartaJugada);
+
+    public void mostrarResponderApuesta();
+
+    public void mostrarEsperandoRespuesta(String jugadorActual);
+
+    public void mostrarDijoQuiero(String jugadorActual);
+
+    public void mostrarDijoNoQuiero(String jugadorActual);
+
+    public void mostrarResultadoEnvido(String ganadorEnvido, String tantos);
+
+    public void mostrarIrseAlMazo(String jugadorActual);
+
+    public void mostrarGanadorRonda(String ganadorRonda);
+
+    public void mostrarGanadorMano(String ganadorMano);
+
+    public void mostrarTurno(boolean esMiTurno);
+
+    public void mostrarFinPartida(String jugadorGanador);
+
+    public void mostrarApuesta(String jugadorActual, Apuesta apuestaActual);
 
 }
